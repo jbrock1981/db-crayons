45f07496cd95050e3a40d7a3a007c765689559b1
Jonathan Brock <jbrock1981@gmail.com>
2018-12-15 10:56:27 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -25,19 +25,63 @@ server.post('/crayons', (req, res) => {
 });
 
 server.get('/crayons', (req, res) => {
-
+    db('crayons')
+        .then(rows => {
+            res
+                .status(201)
+                .json(rows)
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: "Unable to load crayons" })
+        })
 });
 
 server.get('/crayons/:id', (req, res) => {
-
+    const { id } = req.params;
+    db('crayons').where('id', id)
+        .then(crayon => {
+            res
+                .status(201)
+                .json(crayon)
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: "failed to retrieve requested crayon" })
+        })
 });
 
 server.put('/crayons/:id', (req, res) => {
-
+    const { id } = req.params;
+    const crayon = req.body;
+    db('crayons').where('id', id).update(crayon)
+        .then(rowCount => {
+            res
+                .status(201)
+                .json(rowCount)
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: "Unable to update crayon" })
+        })
 });
 
 server.delete('/crayons/:id', (req, res) => {
-
+    const { id } = req.params;
+    db('crayons').where('id', id).del()
+        .then(rowsDeleted => {
+            res
+                .status(201)
+                .json(rowsDeleted)
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: "Unable to delete requested crayon" })
+        })
 });
 
 server.listen(PORT, () => {
